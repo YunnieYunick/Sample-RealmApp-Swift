b3e1bb5e44143dfd3ae836f5c09707944c986a46
Yunnie Yunick <yunnieyunick@gmail.com>
2017-01-22 14:20:14 +0900

    Initial Commit

---
+++ Sample-RealmApp-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sample-RealmApp-Swift
+//
+//  Created by Yunnie Yunick on 2017/01/22.
+//  Copyright © 2017年 yunnieyunick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
